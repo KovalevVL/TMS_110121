cafbca68fc7881b6926035b48a680e9e32ad75a5
unknown <valodb9gn@yandex.ru>
2021-01-18 21:07:51 +0300

    ItSample

---
+++ lesson2/IfSample.java
@@ -0,0 +1,13 @@
+class IfSample {
+public static void main(String args []) {
+int x, y;
+x = 10;
+y = 20;
+if (x<y) System.out.println("x less y");
+x = x * 2;
+if(x==y) System.out.println("x now equal y");
+x = x *2;
+if (x>y) System.out.println("x more y");
+if (x==y) System.out.println("you won't see it");
+}
+}
