7f8587fc3eac7a652172a58de388f9b90f3154d5
unknown <valodb9gn@yandex.ru>
2021-01-18 14:46:04 +0300

    TimePr2

--- lesson2/TimePr2.java
+++ lesson2/TimePr2.java
@@ -4,15 +4,15 @@ int s=4500;
 int sec=s%60;
 int m=(s-sec)/60;
 int min=m%60;
-System.out.println(m + "минут" + sec + "секунд");
+System.out.println(m + "minute" + sec + "second");
 int h=(m-min)/60;
 int hour=h%60;
-System.out.println(hour + "часов" + min + "минут" + sec + "секунд");
+System.out.println(hour + "hour" + min + "minute" + sec + "second");
 int d=(h-hour)/24;
 int day=d % 24;
-System.out.println(day + "дней" + hour + "часов" + min + "минут" + sec + "секунд");
+System.out.println(day + "day" + hour +"hour" + min + "minute" + sec + "second");
 int w=(d-day)/7;
 int week=w % 7;
-System.out.println(week + "недель" + day + "дней" + hour + "часов" + min + "минут" + sec + "секунд");
+System.out.println(week + "week" + day + "day" + hour + "hour" + min + "minute" + sec + "second");
 }
 }
