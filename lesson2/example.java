c32801488b431db8ab5465cc70f5ea88fcd6fa97
KovalevVL <valodb9gn@yandex.ru>
2021-01-22 12:10:24 +0300

    exampleBook

--- lesson2/example.java
+++ lesson2/example.java
@@ -1,10 +1,10 @@
-class Example {
-public static void main(String args[]){
-int num;
-num = 100;
-System.out.println("variable num: "+ num);
-num = num * 2;
-System.out.print("variable value num * 2 equally");
-System.out.println(num);
-}
+class exampleBook {
+    public static void main (String args[]) {
+        int num; // в этой строке кода объявляется переменная с именем num
+        num=250; //в этой строке кода переменной num присваиваем значение 250
+        System.out.println("num: " + num);
+        num = num * 10;
+        System.out.println("num * 10");
+        System.out.println(num);
+    }
 }
