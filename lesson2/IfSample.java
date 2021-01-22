2e261e92be9e0ccf482062bc58aa4d89a5336ed0
KovalevVL <valodb9gn@yandex.ru>
2021-01-22 13:18:17 +0300

    IfSamplebook

---
+++ lesson2/IfSample.java
@@ -0,0 +1,11 @@
+class IfSamplebook {
+    public static void main (String args[]) {
+       int x=10;
+       int y=20;
+        if(x<y)System.out.println("x<y");
+        x=x*2;
+        if(x==y)System.out.println("x=y");
+        x=x*2;
+        if(x>y)System.out.println("x>y");
+    }
+    }
