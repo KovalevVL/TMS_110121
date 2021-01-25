302a850e9750f2da06464e4560eb4a3f98938140
KovalevVL <valodb9gn@yandex.ru>
2021-01-25 15:05:17 +0300

    differenceNum

---
+++ lesson3/src/com/tms/com/NumberTask6.java
@@ -0,0 +1,23 @@
+package com.tms.com;
+
+//Создайте число.
+// Определите, является ли оно трехзначным.
+// Определите, является ли его последняя цифра семеркойю
+// Определите, является ли число четным.
+
+public class NumberTask6 {
+    public static void main(String[] args) {
+        int x = 397;
+        if (99 < x & x <= 999) {
+            System.out.println("x - трехзначное число");
+        }
+        if ( x % 10 == 7){
+            System.out.println("Последняя цифра 7");
+        }
+        if (x % 2 == 0){
+            System.out.println("число х - четное");
+        }else{
+            System.out.println("число х - нечетное");
+        }
+    }
+}
