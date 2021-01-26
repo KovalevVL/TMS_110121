b6fe8f3faa3b1dd9948cf387bc674fcc2ed14ace
KovalevVL <valodb9gn@yandex.ru>
2021-01-27 01:06:28 +0300

    HomeWorkTask6

---
+++ lesson3/src/com/tms/HomeWorkTask6.java
@@ -0,0 +1,16 @@
+package com.tms;
+
+//Даны два числа.
+//Вывести большее из них.
+
+public class HomeWorkTask6 {
+    public static void main (String[] args){
+        int a = 55;
+        int b = 99;
+        if ( a > b ){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
