30c2dcdb47aac14bff85cf6ed3ae6485b508a107
KovalevVL <valodb9gn@yandex.ru>
2021-01-27 00:32:40 +0300

    HomeWorkTask4

---
+++ lesson3/src/com/tms/HomeWorkTask4.java
@@ -0,0 +1,16 @@
+package com.tms;
+
+//Дано целое число.
+//Найти количество положительных чисел в исходном наборе.
+
+
+public class HomeWorkTask4 {
+    public static void main( String[] args ){
+        int a=5;
+        int b=4;
+        int c=44;
+        if ( a > 0 & b > 0 & c > 0) {
+            System.out.println("В исходном наборе три положительных числа");
+        }
+    }
+    }
