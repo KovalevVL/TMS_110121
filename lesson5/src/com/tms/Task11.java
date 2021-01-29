8a3dcbf947d3fbfe5b24372696ff88ed445b9653
KovalevVL <valodb9gn@yandex.ru>
2021-01-30 00:56:03 +0300

    Task11BegginerChapter

---
+++ lesson5/src/com/tms/Task11.java
@@ -0,0 +1,14 @@
+package com.tms;
+
+//Имеется целое число, определить является ли это число простым, т.е делиться без остатка только на 1 и себя.
+
+public class Task11 {
+    public static void main(String[] args) {
+        int x = (int) (Math.random() * 4);
+        if (x % x == 0 & x % 1 == 0) {
+            System.out.println(x + " число является простым");
+        } else {
+            System.out.println(x + " число является составным");
+        }
+    }
+}
