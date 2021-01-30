3edd038b1620bbd1b18c89ef92fecb26610dbae1
KovalevVL <valodb9gn@yandex.ru>
2021-01-30 20:23:08 +0300

    Task14BegginerChapter

---
+++ lesson5/src/com/tms/Task14BegginerChapter.java
@@ -0,0 +1,28 @@
+package com.tms;
+
+//Создать массив оценок произвольной длины, вывести максимальную и минимальную оценку, ее номера.
+
+public class Task14BegginerChapter {
+    public static void main(String args[]) {
+        int array[] = new int[15];
+        int max = 0;
+        int min = 0;
+        int imin = 0;
+        int imax = 0;
+        int i = 1;
+        for (i = 1; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
+            System.out.println("Оценка " + array[i]);
+            if (array[i] < min) {
+                min = array[i];
+                imin = i;
+            } else if (array[i] > max) {
+                max = array[i];
+                imax = i;
+            }
+        }
+        System.out.println("Максимальная оценка " + max + "с индексом" + imax);
+        System.out.println("Минимальная оценка " + min + "с индексом" + imin);
+    }
+}
+
