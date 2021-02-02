bdd7bbe101e7dde5f8e6e6b0aa80badf18fd48cc
KovalevVL <valodb9gn@yandex.ru>
2021-02-03 02:19:20 +0300

    Task15BegginerChapterCorrection

--- lesson5/src/com/tms/Task15BegginerChapter.java
+++ lesson5/src/com/tms/Task15BegginerChapter.java
@@ -6,17 +6,18 @@ public class Task15BegginerChapter {
     public static void main(String args[]) {
         int array[] = new int[6];
         int i;
-        int z;
         for (i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 12);
             System.out.println(array[i]);
         }
+        System.out.println("************************************");
         for (i = 0; i < array.length / 2; i++) {
             int temp = array[array.length - i - 1];
             array[array.length - i - 1] = array[i];
             array[i] = temp;
         }
-        for (i = 0; i < array.length; i++) ;
-        System.out.println(array[i]);
+        for (i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
