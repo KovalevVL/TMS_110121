dcfd28850eed1ad0afd3aa69d8919d1e3598cf34
KovalevVL <valodb9gn@yandex.ru>
2021-02-06 02:42:42 +0300

    HomeWorkTask3Calculate

---
+++ lesson5HomeWork/src/com/tms/HomeWorkTask3.java
@@ -0,0 +1,13 @@
+package com.tms;
+
+//Вычислить:1+2+4+8+......+256
+public class HomeWorkTask3 {
+    public static void main(String[] args) {
+        int a = 1;
+        int sum = 0;
+        for (a = 1; a <= 256; a *= 2) {
+            sum += a;
+        }
+            System.out.println("Сумма чисел равно = " + sum);
+    }
+}
