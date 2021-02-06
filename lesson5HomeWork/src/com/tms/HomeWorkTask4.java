b0d67e5272b1872e9e1fa8c53bb93958619bd516
KovalevVL <valodb9gn@yandex.ru>
2021-02-06 13:39:55 +0300

    HomeWorkTask4SumOfNumbersFor

--- lesson5HomeWork/src/com/tms/HomeWorkTask4.java
+++ lesson5HomeWork/src/com/tms/HomeWorkTask4.java
@@ -6,8 +6,8 @@ public class HomeWorkTask4 {
         int a = 10;
         int b = 100;
         int sum = 0;
-        for (int  i  =1; i<=b; i++){
-            sum+=a;
+        for (int i = 1; i <= b; i++) {
+            sum += a;
         }
         System.out.println(sum);
     }
