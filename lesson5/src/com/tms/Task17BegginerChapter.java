ba78304b4dd483e9be7ef54a4763d4d9a9537c01
KovalevVL <valodb9gn@yandex.ru>
2021-02-07 23:10:40 +0300

    Task17BegginerChapterVarianlesInTheLoop

--- lesson5/src/com/tms/Task17BegginerChapter.java
+++ lesson5/src/com/tms/Task17BegginerChapter.java
@@ -4,21 +4,19 @@ package com.tms;
 public class Task17BegginerChapter {
     public static void main(String args[]) {
         char[][] arr = new char[5][5];
-        int i;
-        int j;
-        for (i = 0; i < arr.length / 2 + 1; i++) {
-            for (j = i; j < arr.length - i; j++) {
+        for (int i = 0; i < arr.length / 2 + 1; i++) {
+            for (int j = i; j < arr.length - i; j++) {
                 arr[i][j] = '1';
             }
         }
-        for (i = arr.length - 1; i > arr.length / 2 - 1; i--) {
-            for (j = i; j > arr.length - i - 2; j--) {
+        for (int i = arr.length - 1; i > arr.length / 2 - 1; i--) {
+            for (int j = i; j > arr.length - i - 2; j--) {
                 arr[i][j] = '1';
             }
         }
 
-        for (i = 0; i < arr.length; i++) {
-            for (j = 0; j < arr[i].length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j] == '\0') {
                     arr[i][j] = '*';
                 }
