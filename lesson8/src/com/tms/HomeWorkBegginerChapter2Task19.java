4d3788c8af0dd7422e5f3aba1c08d354f0509b27
KovalevVL <valodb9gn@yandex.ru>
2021-02-10 16:06:53 +0300

    Task19WordSumAddSpace

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task19.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task19.java
@@ -4,16 +4,18 @@ package com.tms;
 //В начале и в конце текста также могуть быть пробелы, но могут и отсутсвовать.
 public class HomeWorkBegginerChapter2Task19 {
     public static void main(String[] args) {
-        String str = " В Java строка является основным носителем текстовой информации.";
+        String str = " В Java        строка является основным носителем   текстовой информации.";
         int word = 0;
         int i = 0;
-        char symbol;
+        char symbol, symbol1;
         for (i = 0; i < str.length(); i++) {
+            if (i + 1 == str.length())
+                break;
             symbol = str.charAt(i);
-            if (symbol == ' ') {
+            symbol1 = str.charAt((i) + 1);
+            if (symbol == ' ' & symbol1 != ' ') {
                 word++;
             }
-
         }
         System.out.println("Количество слов в строке " + word);
     }
