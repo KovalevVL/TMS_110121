948cf9a1868331cba38d2ba63652f10344a1153d
KovalevVL <valodb9gn@yandex.ru>
2021-02-14 00:51:01 +0300

    Task20LastLetter

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task20.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task20.java
@@ -6,9 +6,9 @@ public class HomeWorkBegginerChapter2Task20 {
         String str = " В Java строка является основным носителем текстовой информации.";
         StringBuilder res = new StringBuilder();
         str = str.trim();
-        String[] words =str.split(" ");
+        String[] words = str.split(" ");
         for (String word : words)
-            res.append(word.toCharArray()[word.length()-1]);
+            res.append(word.toCharArray()[word.length() - 1]);
         System.out.println(res.toString());
 
     }
