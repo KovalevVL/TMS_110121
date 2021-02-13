44e0a904cd550cccfed4006d530a6882d92ee99b
KovalevVL <valodb9gn@yandex.ru>
2021-02-14 00:50:40 +0300

    Task20LastLetter

---
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task20.java
@@ -0,0 +1,15 @@
+package com.tms;
+
+//Имеется строка с текстом. Вывести текс, составленный из последних букв всех слов.
+public class HomeWorkBegginerChapter2Task20 {
+    public static void main(String[] args) {
+        String str = " В Java строка является основным носителем текстовой информации.";
+        StringBuilder res = new StringBuilder();
+        str = str.trim();
+        String[] words =str.split(" ");
+        for (String word : words)
+            res.append(word.toCharArray()[word.length()-1]);
+        System.out.println(res.toString());
+
+    }
+}
