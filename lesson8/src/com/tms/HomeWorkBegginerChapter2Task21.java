3060ea59adc272040e7401e08bb70f0ff5837c19
KovalevVL <valodb9gn@yandex.ru>
2021-02-17 03:57:21 +0300

    Task21Formatting

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task21.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task21.java
@@ -6,23 +6,23 @@ public class HomeWorkBegginerChapter2Task21 {
     public static void main(String[] args) {
         String str = " В Java строка является основным носителем текстовой информации.";
         for (int i = 0; i < 100; i++) {
-            str+=" В Java строка является основным носителем текстовой информации.";
+            str += " В Java строка является основным носителем текстовой информации.";
         }
-        long st=System.currentTimeMillis();
+        long st = System.currentTimeMillis();
         System.out.println(st);
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<100;i++){
+        for (int i = 0; i < 100; i++) {
             sb.append("В Java строка является основным носителем текстовой информации.");
         }
-        long stB= System.currentTimeMillis();
+        long stB = System.currentTimeMillis();
         System.out.println(stB);
-        StringBuffer stBuf= new StringBuffer();
-        for (int i=0; i<100;i++) {
+        StringBuffer stBuf = new StringBuffer();
+        for (int i = 0; i < 100; i++) {
             sb.append("В Java строка является основным носителем текстовой информации.");
         }
-        long strBuf=System.currentTimeMillis();
+        long strBuf = System.currentTimeMillis();
         System.out.println(strBuf);
-        
+
 
     }
 }
