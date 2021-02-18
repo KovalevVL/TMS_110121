4bc6c9594e6d3d2cf64f8f8fa5099ea9d60ffaee
KovalevVL <valodb9gn@yandex.ru>
2021-02-19 01:10:39 +0300

    Task18AddStringRegex

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 //Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
 public class HomeWorkBegginerChapter2Task18 {
     public static void main(String[] args) {
-        String str = "Строка - это упорядоченная последовательность символов. В Java строка является основным носителем текстовой информации. Для работы со строками здесь используются следующие классы: String, StringBuilder, StringBuffer.";
+        String str = "Строка - это упорядоченная последовательность символов. В Java строка является основным носителем текстовой информации. Для работы со строками здесь используются следующие классы: - String, - StringBuilder, - StringBuffer.";
         int n = 0;
         char symbol;
         String regex = "[\\p{Punct}]";
