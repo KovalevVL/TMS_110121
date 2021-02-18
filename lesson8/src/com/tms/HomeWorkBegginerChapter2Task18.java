ded28cff43302b8a12214d261d3b7231a7b69a79
KovalevVL <valodb9gn@yandex.ru>
2021-02-19 01:40:23 +0300

    Task18AddStringRegex

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
@@ -8,7 +8,6 @@ public class HomeWorkBegginerChapter2Task18 {
     public static void main(String[] args) {
         String str = "Строка - это упорядоченная последовательность символов. В Java строка является основным носителем текстовой информации. Для работы со строками здесь используются следующие классы: - String, - StringBuilder, - StringBuffer.";
         int n = 0;
-        char symbol;
         String regex = "[\\p{Punct}]";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
