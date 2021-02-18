058fb37d0aeb7d79799f09ada1907c81c88af5d6
KovalevVL <valodb9gn@yandex.ru>
2021-02-19 01:07:19 +0300

    Task18AddStringRegex

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task18.java
@@ -1,16 +1,19 @@
 package com.tms;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
 public class HomeWorkBegginerChapter2Task18 {
     public static void main(String[] args) {
-        String str = "Строка — это упорядоченная последовательность символов. В Java строка является основным носителем текстовой информации. Для работы со строками здесь используются следующие классы: String, StringBuilder, StringBuffer.";
+        String str = "Строка - это упорядоченная последовательность символов. В Java строка является основным носителем текстовой информации. Для работы со строками здесь используются следующие классы: String, StringBuilder, StringBuffer.";
         int n = 0;
         char symbol;
-        for (int i = 0; i < str.length(); i++) {
-            symbol = str.charAt(i);
-            if (symbol == ',' || symbol == '.' || symbol == ':' || symbol == '—') {
-                n++;
-            }
+        String regex = "[\\p{Punct}]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            n++;
         }
         System.out.println("Общее колизчество знаков препинания " + n);
     }
