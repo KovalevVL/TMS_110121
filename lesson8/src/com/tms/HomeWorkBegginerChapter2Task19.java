e3c1ac10f0911e5c69942a6dd0681c57c4edb7ca
KovalevVL <valodb9gn@yandex.ru>
2021-02-20 18:04:37 +0300

    Task19WordSumAddSpaceRe

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task19.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task19.java
@@ -1,22 +1,20 @@
 package com.tms;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //Имеется строка с тектом. Подсчитать количество слов в тексте. Желательно учесть, что слова могут разделяться несколькими пробелами.
 //В начале и в конце текста также могуть быть пробелы, но могут и отсутсвовать.
 public class HomeWorkBegginerChapter2Task19 {
     public static void main(String[] args) {
-        String str = " В Java        строка является основным носителем   текстовой информации.";
+        String str = "в этой сроке,напрмер, ровно семь слов";
         int word = 0;
-        int i = 0;
-        char symbol, symbol1;
-        for (i = 0; i < str.length(); i++) {
-            if (i + 1 == str.length())
-                break;
-            symbol = str.charAt(i);
-            symbol1 = str.charAt((i) + 1);
-            if (symbol == ' ' & symbol1 != ' ') {
-                word++;
-            }
-        }
-        System.out.println("Количество слов в строке " + word);
+        String regex = "[\\pP\\s]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find())
+            word++;
+            System.out.println("количество слов в предложении " +word);
     }
 }
+
