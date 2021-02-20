4bfe8d4410ab8a5be54df8ae153df19bc0729fbd
KovalevVL <valodb9gn@yandex.ru>
2021-02-20 23:45:09 +0300

    Task20LastLit

--- lesson8/src/com/tms/HomeWorkBegginerChapter2Task20.java
+++ lesson8/src/com/tms/HomeWorkBegginerChapter2Task20.java
@@ -1,15 +1,17 @@
 package com.tms;
 
+
 //Имеется строка с текстом. Вывести текс, составленный из последних букв всех слов.
 public class HomeWorkBegginerChapter2Task20 {
     public static void main(String[] args) {
-        String str = " В Java строка является основным носителем текстовой информации.";
-        StringBuilder res = new StringBuilder();
+        String str = " В Java - строка является      основным носителем текстовой информации.";
+        str = str.replaceAll("\\pP", " ");
         str = str.trim();
-        String[] words = str.split(" ");
-        for (String word : words)
-            res.append(word.toCharArray()[word.length() - 1]);
-        System.out.println(res.toString());
-
+        String[] str1 = str.split(" +");
+        String lit = "";
+        for (String word : str1) {
+            lit += word.toCharArray()[word.length() - 1];
+        }
+        System.out.println(lit);
     }
 }
