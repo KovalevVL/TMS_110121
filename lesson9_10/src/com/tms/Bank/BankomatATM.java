54dccc0ad98e232a2685903d5b8ebf85000518d1
KovalevVL <valodb9gn@yandex.ru>
2021-03-21 01:10:52 +0300

    BankomatCorrect

--- lesson9_10/src/com/tms/Bank/BankomatATM.java
+++ lesson9_10/src/com/tms/Bank/BankomatATM.java
@@ -10,75 +10,55 @@ package com.tms.Bank;
 //Создание класса
 
 public class BankomatATM {
-    private int twenty = 20;
-    private int fifty = 50;
-    private int hundred = 100;
-    public int i = 1;
-    private int sunMoney;
-    boolean exitMoney = true;
-    boolean addMoney = true;
-
+    private static int x20 = 20;
+    private static int x50 = 20;
+    private static int x100 = 100;
+    public int sunMoney;
+    private boolean getMoney = true;
 
     //конструктор
 
-    BankomatATM(int tw, int f, int h) {
-        this.twenty = 20;
-        this.fifty = 50;
-        this.hundred = 100;
-        sunMoney();
-
+    BankomatATM(int x20, int x50, int x100) {
+        this.x20 = x20;
+        this.x50 = x50;
+        this.x100 = x100;
     }
     // Определение суммы
 
-    private void sunMoney() {
-        sunMoney = this.hundred * 100 + this.fifty * 50 + this.twenty * 20;
+    private void MoneyResult() {
+        sunMoney = this.x100 * 100 + this.x50 * 50 + this.x20 * 20;
     }
 
     //Ввод денег в банкомат
 
-    protected void moneyAdd(int moneySumAdd) {
-        for (i = 1; i < moneySumAdd / 100; i++) {
-        }
-        if (moneySumAdd % 100 == 0) {
-            addMoney = true;
-            System.out.println("Внесено " + moneySumAdd + " долларов " + i + " купюрами, номиналом " + hundred);
-        } else if (moneySumAdd % 50 == 0) {
-            addMoney = true;
-            System.out.println("Внесено " + moneySumAdd + " долларов " + i + " купюрами, номиналом " + fifty);
-        } else if (moneySumAdd % 20 == 0) {
-            addMoney = true;
-            System.out.println("Внесено " + moneySumAdd + " долларов " + i + " купюрами, номиналом " + twenty);
-        } else {
-            System.out.println("Внесенная купюра не принята.");
-        }
+    public void moneyAdd(int banknota20, int banknota50, int banknota100) {
+        sunMoney = x20 * 20 + x50 * 50 + x100 * 100;
+        int moneyAddSun = banknota20 * 20 + banknota50 * 50 + banknota100 * 100;
+        sunMoney += moneyAddSun;
+        x20 += banknota20;
+        x50 += banknota50;
+        x100 += banknota100;
+        System.out.println("Вы внесли денежные средства на сумму " + moneyAddSun);
     }
-
 
     //Вывод денег из банкомата
-    protected void remMoney(int moneySumRem) {
-        for (i = 1; i < moneySumRem / 100; i++) {
-        }
-        if (moneySumRem % 100 == 0) {
-            exitMoney = true;
-            System.out.println("Вы сняли " + moneySumRem + " долларов " + i + " купюрами, номиналом " + hundred);
-        } else if (moneySumRem % 50 == 0) {
-            exitMoney = true;
-            System.out.println("Вы сняли " + moneySumRem + " долларов " + i + " купюрами, номиналом " + fifty);
-        } else if (moneySumRem % 20 == 0) {
-            exitMoney = true;
-            System.out.println("Вы сняли " + moneySumRem + " долларов " + i + " купюрами, номиналом " + twenty);
+
+    public void moneyAsk(int requestCash) {
+        if (sunMoney >= requestCash) {
+            getMoney = true;
+            System.out.println("Операция  удалась");
         } else {
-            System.out.println("Данную сумму снять не возможно!");
+            getMoney = false;
+            System.out.println("Операция не удлась");
         }
     }
 
-    //вывод данных
-    public void printResult() {
-        if (exitMoney) {
-            System.out.println("Операция прошла успешно");
-        } else {
-            System.out.println("Ошибка! Попробуйте еще раз.");
-            exitMoney = true;
-        }
+    public void bank() {
+        System.out.println("Количество денег в банкомате " + sunMoney);
+        System.out.println("Количество купюр номиналом 20 = " + x20 + ", на сумму " + x20 * 20);
+        System.out.println("Количество купюр номиналом 50 = " + x50 + ", на сумму " + x50 * 50);
+        System.out.println("Количество купюр номиналом 100 = " + x100 + ", на сумму " + x100 * 100);
+        return;
     }
 }
+
--- lesson9_10/src/com/tms/Bank/RunnerATM.java
+++ lesson9_10/src/com/tms/Bank/RunnerATM.java
@@ -2,19 +2,10 @@ package com.tms.Bank;
 
 public class RunnerATM {
     public static void main(String[] args) {
-        BankomatATM a = new BankomatATM (10, 10 ,10);
-        a.moneyAdd(20);
-        a.moneyAdd(100);
-        a.moneyAdd(50);
-
-        a.remMoney(400);
-        a.remMoney(60);
-        a.remMoney(700);
-
-
-
-        a.printResult();
-
+     BankomatATM atm = new BankomatATM(10,10,10);
+     atm.moneyAdd(5,3,1);
+     atm.moneyAsk(100);
+     atm.bank();
     }
 
 }
