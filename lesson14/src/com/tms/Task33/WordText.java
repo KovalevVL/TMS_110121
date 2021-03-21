7f9d12726f67a0b55f2eac187cc9d5adae278e8e
KovalevVL <valodb9gn@yandex.ru>
2021-03-22 02:53:27 +0300

    WordTextCorrect

--- lesson14/src/com/tms/Task33/WordText.java
+++ lesson14/src/com/tms/Task33/WordText.java
@@ -13,24 +13,23 @@ import java.util.regex.Pattern;
 public class WordText {
 
     public static void main(String[] args) throws Exception {
-        String TextNameFile = new String("D:\\VL_it\\TMS_110121\\lesson14\\src\\com\\tms\\Task33\\lesson14");
+        String TextNameFile = new String("D:\\VL_it\\TMS_110121\\lesson14\\src\\com\\tms\\Task33\\Text.txt");
         BufferedReader tx = new BufferedReader(new FileReader(TextNameFile));
         Scanner sc = new Scanner(new FileInputStream(TextNameFile));
         int punct = 0;
         int word = 0;
-        int res = tx.read();
         while (sc.hasNext()) {
             sc.next();
             word++;
         }
-        Pattern pattern = Pattern.compile("[.,:;?!()-]");
         Scanner scanner = new Scanner(new FileInputStream(TextNameFile));
+        Pattern pattern = Pattern.compile("\\p{Punct}");
         Matcher matcher = pattern.matcher(scanner.nextLine());
         while (matcher.find()) {
             punct++;
         }
 
-        System.out.println(word);
-        System.out.println(punct);
+        System.out.println("Количество слов в тексте " +word);
+        System.out.println ("Количество знаков препинания в тексте " +punct);
     }
 }
